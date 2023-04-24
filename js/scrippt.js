b138549b4c0a7318fe4188f157fd99f78c1695e2
K-nataliaG <nataliagualdron11@gmail.com>
2023-04-24 16:06:55 -0500

    actualizado

---
+++ js/scrippt.js
@@ -0,0 +1,68 @@
+function comprobar() {
+    let nompro, linpro, verant, veract, modusu, modobje, desgepro, obje, arqui, 
+    serhard, sersoft, serotro, clihard, clisoft, cliotro, requefungene, requeadi, clipro, versi, 
+    natura, fechaela, fechavali, versi2, natura2, fechaela2, fechavali2, firmapro, 
+    firmvali, fechaapro, fechavalido
+
+    nompro=document.getElementById("nompro").value
+    linpro=document.getElementById("linpro").value
+    verant=document.getElementById("verant").value
+    veract=document.getElementById("veract").value
+    modusu=document.getElementById("modusu").value
+    modobje=document.getElementById("modobje").value
+    desgepro=document.getElementById("desgepro").value
+    obje=document.getElementById("obje").value
+    arqui=document.getElementById("arqui").value
+    serhard=document.getElementById("serhard").value
+    sersoft=document.getElementById("sersoft").value
+    serotro=document.getElementById("serotro").value
+    clihard=document.getElementById("clihard").value
+    clisoft=document.getElementById("clisoft").value
+    cliotro=document.getElementById("cliotro").value
+    requefungene=document.getElementById("requefungene").value
+    requeadi=document.getElementById("requeadi").value
+    clipro=document.getElementById("clipro").value
+    versi=document.getElementById("versi").value
+    natura=document.getElementById("natura").value
+    fechaela=document.getElementById("fechaela").value
+    fechavali=document.getElementById("fechavali").value
+    versi2=document.getElementById("versi2").value
+    natura2=document.getElementById("natura2").value
+    fechaela2=document.getElementById("fechaela2").value
+    fechavali2=document.getElementById("fechavali2").value
+    firmapro=document.getElementById("firmapro").value
+    firmvali=document.getElementById("firmvali").value
+    fechaapro=document.getElementById("fechaapro").value
+    fechavalido=document.getElementById("fechavalido").value
+
+    document.getElementById("respuesta").innerHTML="<h5> "+nompro+"</h5>"
+    document.getElementById("respuesta2").innerHTML="<h5> "+linpro+"</h5>"
+    document.getElementById("respuesta3").innerHTML="<h5> "+verant+"</h5>"
+    document.getElementById("respuesta4").innerHTML="<h5> "+veract+"</h5>"
+    document.getElementById("respuesta5").innerHTML="<h5> "+modusu+"</h5>"
+    document.getElementById("respuesta6").innerHTML="<h5> "+modobje+"</h5>"
+    document.getElementById("respuesta7").innerHTML="<h5> "+desgepro+"</h5>"
+    document.getElementById("respuesta8").innerHTML="<h5> "+obje+"</h5>"
+    document.getElementById("respuesta9").innerHTML="<h5> "+arqui+"</h5>"
+    document.getElementById("respuesta10").innerHTML="<h5> "+serhard+"</h5>"
+    document.getElementById("respuesta11").innerHTML="<h5> "+sersoft+"</h5>"
+    document.getElementById("respuesta12").innerHTML="<h5> "+serotro+"</h5>"
+    document.getElementById("respuesta13").innerHTML="<h5> "+clihard+"</h5>"
+    document.getElementById("respuesta14").innerHTML="<h5> "+clisoft+"</h5>"
+    document.getElementById("respuesta15").innerHTML="<h5> "+cliotro+"</h5>"
+    document.getElementById("respuesta16").innerHTML="<h5> "+requefungene+"</h5>"
+    document.getElementById("respuesta17").innerHTML="<h5> "+requeadi+"</h5>"
+    document.getElementById("respuesta18").innerHTML="<h5> "+clipro+"</h5>"
+    document.getElementById("respuesta19").innerHTML="<h5> "+versi+"</h5>"
+    document.getElementById("respuesta20").innerHTML="<h5> "+natura+"</h5>"
+    document.getElementById("respuesta21").innerHTML="<h5> "+fechaela+"</h5>"
+    document.getElementById("respuesta22").innerHTML="<h5> "+fechavali+"</h5>"
+    document.getElementById("respuesta23").innerHTML="<h5> "+versi2+"</h5>"
+    document.getElementById("respuesta24").innerHTML="<h5> "+natura2+"</h5>"
+    document.getElementById("respuesta25").innerHTML="<h5> "+fechaela2+"</h5>"
+    document.getElementById("respuesta26").innerHTML="<h5> "+fechavali2+"</h5>"
+    document.getElementById("respuesta27").innerHTML="<h5> "+firmapro+"</h5>"
+    document.getElementById("respuesta28").innerHTML="<h5> "+firmvali+"</h5>"
+    document.getElementById("respuesta29").innerHTML="<h5> "+fechaapro+"</h5>"
+    document.getElementById("respuesta30").innerHTML="<h5> "+fechavalido+"</h5>"
+}
